f2120c4df8ff10b2e12697a613c46679a3bb3d38
TRUETECHCODE <terveraosu@gmail.com>
2023-05-24 21:31:29 +0100

    Rename entity from Headline to Article: To avoid comflict witht the app name, Models and Controllers with all helper files named headline is renamed to article

--- app/controllers/headlines_controller.rb
+++
@@ -1,3 +0,0 @@
-class HeadlinesController < ApplicationController
-def index; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "headlines#index"
+  root "articles#index"
 
   resources :users
 end
