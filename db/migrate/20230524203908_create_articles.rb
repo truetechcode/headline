74e7d56226f34719a78a39263effadec9a9e32c0
TRUETECHCODE <terveraosu@gmail.com>
2023-05-29 15:58:57 +0100

    create articles table associated columns

---
+++ db/migrate/20230524203908_create_articles.rb
@@ -0,0 +1,17 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :source
+      t.string :author
+      t.string :title
+      t.text :description
+      t.text :content
+      t.string :url
+      t.string :url_to_image
+      t.string :publish_at
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_104251) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_24_203908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "source"
+    t.string "author"
+    t.string "title"
+    t.text "description"
+    t.text "content"
+    t.string "url"
+    t.string "url_to_image"
+    t.string "publish_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_104251) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
