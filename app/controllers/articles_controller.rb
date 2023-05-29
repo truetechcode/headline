7ede5990362c6ff65741968e725eb40f624d8827
TRUETECHCODE <terveraosu@gmail.com>
2023-05-29 17:54:58 +0100

    Add index, new and create actions to the ArticlesController.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,23 @@
 class ArticlesController < ApplicationController
 def index; end
+
+def new; end
+
+def create
+  @article = Article.new(articles_params)
+  @article.user = User.last
+  if @article.save
+    flash[:success] = "Article successfully saved"
+    redirect_to root_path
+  else
+    flash[:error] = @article.errors.full_messages[0]
+    render 'new'
+  end
+end
+
+protected
+
+def articles_params
+  params.require(:article).permit(:source, :author, :title, :description, :content, :url, :url_to_image, :publish_at, :user)
+end
 end
