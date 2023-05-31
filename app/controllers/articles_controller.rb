417189c46ce158a19d2940f9e9bc532739c414b0
TRUETECHCODE <terveraosu@gmail.com>
2023-05-31 20:20:15 +0100

    Implement destroy action for articles with flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,11 +10,22 @@ def create
   @article = Article.new(articles_params)
   @article.user = User.last
   if @article.save
-    flash[:success] = "Article successfully saved"
+    flash[:success] = "Headline successfully saved"
     redirect_to root_path
   else
     flash[:error] = @article.errors.full_messages[0]
     render 'new'
+  end
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  if @article.destroy
+    flash[:success] = 'Headline was successfully deleted.'
+    redirect_to articles_url
+  else
+    flash[:error] = @article.errors.full_messages[0]
+    redirect_to articles_url
   end
 end
 
