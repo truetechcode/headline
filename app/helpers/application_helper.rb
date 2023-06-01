ba1518585dbe92ff79395f79582f54e880359c97
TRUETECHCODE <terveraosu@gmail.com>
2023-06-01 15:50:35 +0100

    Add placeholder_image_news helper method to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def placeholder_image_news(size: '300x150')
+    Faker::Placeholdit.image(size: size, format: 'jpeg', background_color: :random, text: 'Headlines')
+  end
 end
