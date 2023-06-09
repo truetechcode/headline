4c90580db3f5ce718efd575b24841c92dc2034d7
TRUETECHCODE <terveraosu@gmail.com>
2023-06-09 16:37:14 +0100

    Refactor articles request tests examples with sessions

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   describe "GET /index" do
+    before(:each) do
+      create(:user, name: 'user', email: 'user@example.com', password: 'password', password_confirmation: 'password')
+      post sessions_path, params: { session: {email: 'user@example.com', password: 'password'} }
+    end
+
     it 'renders Headlines' do
       @live_articles = []
       get articles_path
@@ -9,10 +14,10 @@ RSpec.describe "Articles", type: :request do
       expect(response).to have_http_status(:success)
       expect(response.body).to include('Headlines')
     end
+
     context "with at least one saved article" do
       before(:each) do
-        create(:user)
-        @article = create(:article)
+        @article = create(:article, user: User.last)
       end
 
       it 'renders article source' do
@@ -47,37 +52,36 @@ RSpec.describe "Articles", type: :request do
       it 'returns a message: No Headlines' do
         get articles_path
 
-        expect(Article.all.count).to eq(0)
+        expect(Article.count).to eq(0)
         expect(response.body).to include('No Headlines')
       end
     end
   end
 
   describe 'POST /articles' do
+    before(:each) do
+      create(:user, name: 'user', email: 'user@example.com', password: 'password', password_confirmation: 'password')
+      post sessions_path, params: { session: {email: 'user@example.com', password: 'password'} }
+    end
+
     context "with valid attributes" do
-      it "creates a new article" do
+      before(:each) do
         create(:user)
         article_attributes = FactoryBot.attributes_for(:article)
         post articles_path, params: { article: article_attributes }
+      end
 
+      it "creates a new article" do
         new_record = Article.last
-        expect(Article.all.count).to eq(1)
+        expect(Article.count).to eq(1)
       end
 
       it "redirects to the correct path" do
-        create(:user)
-        article_attributes = FactoryBot.attributes_for(:article)
-        post articles_path, params: { article: article_attributes }
-
         expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to(root_path)
       end
 
       it "returns a successful response with the expected content" do
-        create(:user)
-        article_attributes = FactoryBot.attributes_for(:article)
-        post articles_path, params: { article: article_attributes }
-
         follow_redirect!
         expect(response).to have_http_status(:success)
         expect(response.body).to include('Headline successfully saved')
@@ -85,23 +89,32 @@ RSpec.describe "Articles", type: :request do
     end
 
     context "with invalid attributes" do
-      it "returns an error for no user" do
-        article_attributes = FactoryBot.attributes_for(:article)
-        post articles_path, params: { article: article_attributes }
+      before(:each) do
+        create(:user, name: 'user', email: 'user@example.com', password: 'password', password_confirmation: 'password')
+        post sessions_path, params: { session: {email: 'user@example.com', password: 'password'} }
+      end
 
-        new_record = Article.last
-        expect(Article.all.count).to eq(0)
-        expect(response.body).to include("User must exist")
+      it "returns an error for no user" do
+        article = build(:article, user: nil)
+        article.save
+
+        expect(article).not_to be_valid
+        expect(article.errors[:user]).to include("must exist")
       end
     end
   end
 
   describe 'DELETE /articles' do
     let!(:article) { FactoryBot.create(:article) }
+    before(:each) do
+      create(:user, name: 'user', email: 'user@example.com', password: 'password', password_confirmation: 'password')
+      post sessions_path, params: { session: {email: 'user@example.com', password: 'password'} }
+      @article = create(:article, user: User.last)
+    end
 
     it 'deletes the article' do
       expect {
-        delete article_path(article)
+        delete article_path(@article)
       }.to change(Article, :count).by(-1)
 
     end
