4694eb06b8b665dd090fdb0639ee01ef0dac3c2b
TRUETECHCODE <terveraosu@gmail.com>
2023-06-11 19:12:27 +0100

    Add country_code to users_params in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
 def user_params
-  params.require(:user).permit(:name, :email, :password)
+  params.require(:user).permit(:name, :email, :password, :country_code)
 end
 end
