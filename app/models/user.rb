e63ebd0df3ba0cca123b527ecf8ee6a5eb23d218
TRUETECHCODE <terveraosu@gmail.com>
2023-06-11 19:22:04 +0100

    Refactor user model to add country code validation and before save callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   has_many :articles
 
   before_save { |user| user.email = email.downcase }
+  before_save { |user| user.country_code = country_code.downcase }
   before_save :create_remember_token
 
   validates :name, presence: true
   validates :email, presence: true
+  validates :country_code, presence: true
 
   validates_length_of :password, minimum: 5, presence: true
 
