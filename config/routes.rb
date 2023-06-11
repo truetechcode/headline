d314d2c513caf44bab076ab533285e2d7c627bb7
TRUETECHCODE <terveraosu@gmail.com>
2023-06-11 19:26:57 +0100

    Add country collection to Articles resources route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :users
-  resources :articles
+  resources :articles do
+    collection  do
+      get ':country', to: 'articles#index', as: 'country'
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
 end
