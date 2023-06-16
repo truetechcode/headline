4c3165ef3b7c65a20ef354abb5c1736d818da27b
TRUETECHCODE <terveraosu@gmail.com>
2023-06-16 19:44:46 +0100

    Refactor user model, add condition in before save callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   has_many :articles
 
-  before_save { |user| user.email = email.downcase }
+  before_save { |user| user.email = email.downcase if user.email.present? }
   before_save { |user| user.country_code = country_code.downcase }
   before_save :create_remember_token
 
   validates :name, presence: true
+  validates :email, presence: true
   validates :country_code, presence: true
-  validates_uniqueness_of :email, presence: true
+  validates_uniqueness_of :email
   validates_length_of :password, minimum: 5, presence: true
 
   private
