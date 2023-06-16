2554c8f2a08840e4f3257751fc6ddabcc56ee07e
TRUETECHCODE <terveraosu@gmail.com>
2023-06-16 23:13:35 +0100

    Add Api scoped Users resources routes for json response

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :users
+  # resources :users
   resources :articles do
     collection  do
       get ':country', to: 'articles#index', as: 'country'
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users, defaults: { format: :html }
+
+  scope '/api', defaults: { format: :json } do
+    resources :users, only: [:create]
+  end
 end
