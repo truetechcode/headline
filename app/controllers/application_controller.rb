93e98a27c8c753d87a391b9c8350c1cc6156d559
TRUETECHCODE <terveraosu@gmail.com>
2023-06-17 09:22:37 +0100

    Add skip_before_action to skip verify_authenticity_token for api request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: :api_request?
   include SessionsHelper
 
   def handle_unverified_request
     sign_out
     super
+  end
+
+  private
+  def api_request?
+    request.format.json?
   end
 end
