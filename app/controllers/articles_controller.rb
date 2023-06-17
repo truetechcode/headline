06c028c2da1601919285c1c7e827e4dd4a8f24dd
TRUETECHCODE <terveraosu@gmail.com>
2023-06-17 09:29:08 +0100

    Refactor ArticlesController to accept json request

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,34 +6,65 @@ def index
   @country_code = (params[:country] || current_user.country_code).downcase
   news_service = NewsApi.new(@country_code)
   @live_articles = news_service.call || []
+
+  respond_to do |format|
+    format.html { render }
+    format.json { render json: { saved_headlines: @articles, headlines: @live_articles }, status: :ok }
+  end
 end
 
 def new; end
 
 def create
   @article = current_user.articles.build(articles_params)
-  @article.user = User.last
+  @article.user = current_user
   if @article.save
     Rails.logger.info("Headline successfully saved")
     flash[:success] = "Headline successfully saved"
-    redirect_to root_path
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { render json: { message: 'Headline successfully saved', headline: @article }, status: :created }
+    end
   else
-    Rails.logger.error(@article.errors.full_messages[0])
-    flash[:error] = @article.errors.full_messages[0]
-    render 'new'
+    Rails.logger.error(@article.errors.full_messages.join)
+    flash[:error] = @article.errors.full_messages.join
+
+    respond_to do |format|
+      format.html { render 'new' }
+      format.json { render json: { error: @article.errors.full_messages.join }, status: :unprocessable_entity  }
+    end
   end
 end
 
 def destroy
-  @article = Article.find(params[:id])
-  if @article.destroy
+  @article = Article.find_by(id: params[:id])
+  if !@article.nil? and @article.destroy
     Rails.logger.info("Headline was successfully deleted.")
     flash[:success] = 'Headline was successfully deleted.'
-    redirect_to articles_url
+
+    respond_to do |format|
+      format.html { redirect_to articles_url }
+      format.json { render json: { message: 'Headline successfully deleted', headline: nil } }
+    end
   else
-    Rails.logger.error(@article.errors.full_messages[0])
-    flash[:error] = @article.errors.full_messages[0]
-    redirect_to articles_url
+    if @article.nil?
+      Rails.logger.error("Headline not found")
+      flash[:error] = "Headline not found"
+
+      respond_to do |format|
+        format.html { redirect_to articles_url }
+        format.json { render json: { error: "Headline not found" }, status: :unprocessable_entity  }
+      end
+    else
+      Rails.logger.error(@article.errors.full_messages.join)
+      flash[:error] = @article.errors.full_messages.join
+
+      respond_to do |format|
+        format.html { redirect_to articles_url }
+        format.json { render json: { error: @article.errors.full_messages.join }, status: :unprocessable_entity  }
+      end
+    end
   end
 end
 
