14668dcf904299f67b49a2caa822e9d12c6e45c7
TRUETECHCODE <terveraosu@gmail.com>
2023-06-20 04:13:21 +0100

    Correct RuboCop RSpec/InstanceVariable offenses

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,37 +3,39 @@
 require "rails_helper"
 
 RSpec.describe "Articles" do
-  before do
-    @articles = [
+  let(:articles) do
+    [
       {
         source: { id: "associated-press", name: "Associated Press" },
         author: "Susie Blann",
         title: "Russian private army head claims control of Bakhmut but Ukraine says fighting continues - The Associated
-         Press",
+       Press",
         description: "KYIV, Ukraine (AP) — The head of the Russian private army Wagner claimed Saturday that his forces
-        have taken control of the city of Bakhmut after the longest and most grinding battle of the Russia-Ukraine war,
-        but Ukrainian defense officials denied it.",
+      have taken control of the city of Bakhmut after the longest and most grinding battle of the Russia-Ukraine war,
+      but Ukrainian defense officials denied it.",
         url: "https://apnews.com/article/bakhmut-russia-ukraine-wagner-prigozhin-da2fc05b818b3dcc39decd40b17d2d8b",
         url_to_image: "https://storage.googleapis.com/afs-prod/media/8dd9fdbbef9d4727a480b275e08fe599/2879.webp",
         publish_at: "2023-05-20T15:37:42Z",
         content: "KYIV, Ukraine (AP) The head of the Russian private army Wagner claimed Saturday that his forces have
-         taken control of the city of Bakhmut after the longest and most grinding battle of the Russia-Ukra…"
+       taken control of the city of Bakhmut after the longest and most grinding battle of the Russia-Ukra…"
       },
       {
         source: { id: nil, name: "POLITICO.eu" },
         author: "Ashleigh Furlong",
         title: "Russia warns West sending F-16s to Ukraine 'carries enormous risks': TASS - POLITICO Europe",
         description: "Kyiv has pushed its allies to supply modern combat aircraft for Ukraine’s defense against the
-        Russian invasion.",
+      Russian invasion.",
         url: "https://www.politico.eu/article/russia-alexander-grushko-warns-west-f16-jets-ukraine-carries-enormous-risks-tass/",
         url_to_image: "https://www.politico.eu/cdn-cgi/image/width=1200,height=630,fit=crop,quality=80,onerror=redirect/wp-content/uploads/2023/05/20/GettyImages-1488520640-scaled.jpg",
         publish_at: "2023-05-20T15:22:00Z",
         content: "The Wests effort to potentially send modern fighter jets to Ukraine carries enormous risks, Russias
-        Deputy Foreign Minister Alexander Grushko warned on Saturday, according to Russian state news agenc…"
+      Deputy Foreign Minister Alexander Grushko warned on Saturday, according to Russian state news agenc…"
       }
     ]
+  end
 
-    allow_any_instance_of(NewsApi).to receive(:call).and_return(@articles)
+  before do
+    allow_any_instance_of(NewsApi).to receive(:call).and_return(articles)
 
     create(:user, name: "user", email: "user@example.com", password: "password", password_confirmation: "password")
     post sessions_path, params: { session: { email: "user@example.com", password: "password" } }
@@ -50,32 +52,32 @@ RSpec.describe "Articles" do
     end
 
     context "with at least one saved article" do
-      before do
-        @article = create(:article, source: @articles[0][:source][:name], author: @articles[0][:author],
-                                    title: @articles[0][:title], description: @articles[0][:description],
-                                    url: @articles[0][:url], url_to_image: @articles[0][:url_to_image],
-                                    publish_at: @articles[0][:publish_at], content: @articles[0][:content],
-                                    user: User.last)
+      let(:article) do
+        create(:article, source: articles[0][:source][:name], author: articles[0][:author],
+                         title: articles[0][:title], description: articles[0][:description],
+                         url: articles[0][:url], url_to_image: articles[0][:url_to_image],
+                         publish_at: articles[0][:publish_at], content: articles[0][:content],
+                         user: User.last)
       end
 
       it "renders article source" do
-        expect(response.body).to include(@article["source"])
+        expect(response.body).to include(article["source"])
       end
 
       it "renders article Title" do
-        expect(response.body).to include(@article["title"])
+        expect(response.body).to include(article["title"])
       end
 
       it "renders article url" do
-        expect(response.body).to include(@article["url"])
+        expect(response.body).to include(article["url"])
       end
 
       it "renders article url_to_image" do
-        expect(response.body).to include(URI.parse(@article["url_to_image"]).host)
+        expect(response.body).to include(URI.parse(article["url_to_image"]).host)
       end
 
       it "renders article publish_at" do
-        expect(response.body).to include(@article["publish_at"])
+        expect(response.body).to include(article["publish_at"])
       end
     end
 
@@ -124,15 +126,11 @@ RSpec.describe "Articles" do
   end
 
   describe "DELETE /articles" do
-    let!(:article) { create(:article) }
-
-    before do
-      @article = create(:article, user: User.last)
-    end
+    let!(:article) { create(:article, user: User.last) }
 
     it "deletes the article" do
       expect do
-        delete article_path(@article)
+        delete article_path(article)
       end.to change(Article, :count).by(-1)
     end
   end
