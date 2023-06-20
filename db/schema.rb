626ba8dcb1f17caac107492784dfbdcb1733b514
TRUETECHCODE <terveraosu@gmail.com>
2023-06-20 01:34:17 +0100

    Autocorrect more RuboCop Rails/UniqueValidationWithoutIndex offenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_170034) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_003047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_170034) do
     t.string "country_code"
     t.index ["country"], name: "index_users_on_country"
     t.index ["country_code"], name: "index_users_on_country_code"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
