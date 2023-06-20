6af0b1215b0ea8993605dc137a9e7a8329ac75a7
TRUETECHCODE <terveraosu@gmail.com>
2023-06-20 02:12:21 +0100

    Correct RuboCop Rails/HelperInstanceVariable offenses in SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,11 +16,11 @@ module SessionsHelper
   end
 
   def current_user=(user)
-    @current_user = user
+    request.env[:current_user] = user
   end
 
   def current_user
-    @current_user ||= User.find_by(remember_token: cookies[:remember_token])
+    request.env[:current_user] ||= User.find_by(remember_token: cookies[:remember_token])
   end
 
   def signed_in_user
