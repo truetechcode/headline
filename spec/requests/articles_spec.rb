21cff883a6c288b6fbee7b93a1c60fc857970c21
TRUETECHCODE <terveraosu@gmail.com>
2023-06-22 14:05:03 +0100

    Refactor test specs to work with dotenv gem

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Articles" do
   let(:instance) { NewsApi.new("us") }
 
   before do
-    stub_request(:get, "https://newsapi.org/v2/top-headlines?apiKey=#{ENV.fetch(:NEWSAPI_KEY, nil)}&country=us")
+    stub_request(:get, "https://newsapi.org/v2/top-headlines?apiKey=#{ENV.fetch('NEWSAPI_KEY', nil)}&country=us")
       .with(
         headers: {
           "Accept" => "*/*",
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe "Sessions" do
     let(:instance) { NewsApi.new("us") }
 
     before do
-      stub_request(:get, "https://newsapi.org/v2/top-headlines?apiKey=#{ENV.fetch(:NEWSAPI_KEY, nil)}&country=us")
+      stub_request(:get, "https://newsapi.org/v2/top-headlines?apiKey=#{ENV.fetch('NEWSAPI_KEY', nil)}&country=us")
         .with(
           headers: {
             "Accept" => "*/*",
