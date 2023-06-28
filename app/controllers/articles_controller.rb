a015033ef0c20fad1af2794b0b8492b92dca9746
TRUETECHCODE <terveraosu@gmail.com>
2023-06-28 13:17:34 +0100

    Refactor ArticlesController and index view to fix code smell

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,42 +5,47 @@ class ArticlesController < ApplicationController
   before_action :signed_in_user
 
   def index
-    @articles = current_user.articles
-    @country_code = (params[:country] || current_user.country_code).downcase
-    news_service = NewsApi.new(@country_code)
-    @live_articles = news_service.call || []
+    articles = current_user.articles
+    country_code = fetch_country_code
+    news_service = NewsApi.new(country_code)
+    live_articles = news_service.call || []
 
     respond_to do |format|
-      format.html { render }
-      format.json { render json: { saved_headlines: @articles, headlines: @live_articles }, status: :ok }
+      format.html do
+        render locals: { articles:, live_articles:, country_code: }
+      end
+      format.json { render json: { saved_headlines: articles, headlines: live_articles }, status: :ok }
     end
   end
 
   def new; end
 
   def create
-    @article = build_article
-    if save_article
-      handle_successful("saved", :created)
+    article = build_article
+    if article.save
+      handle_successful("saved", :created, article)
     else
-      handle_failed
+      handle_failed article.errors.full_messages.join
     end
   end
 
   def destroy
-    @article = Article.find_by(id: params[:id])
-    if !@article.nil? && @article.destroy
+    article = Article.find_by(id: params[:id])
 
-      handle_successful("deleted", nil)
-
-    elsif @article.nil?
-      handle_not_found
+    if article&.destroy
+      handle_successful("deleted", nil, article)
+    elsif article.nil?
+      handle_not_found "Headline not found"
     else
-      handle_failed
+      handle_failed article.errors.full_messages.join
     end
   end
 
   private
+
+  def fetch_country_code
+    (params[:country] || current_user.country_code).downcase
+  end
 
   def build_article
     current_user.articles.build(articles_params).tap do |article|
@@ -48,11 +53,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def save_article
-    @article.save
-  end
-
-  def handle_successful(item, status)
+  def handle_successful(item, status, article)
     Rails.logger.info("Headline successfully #{item}")
 
     respond_to do |format|
@@ -60,34 +61,32 @@ class ArticlesController < ApplicationController
         flash[:success] = "Headline successfully #{item}"
         redirect_to root_path
       end
-      format.json { render json: { message: "Headline successfully #{item}", headline: @article }, status: }
+      format.json { render json: { message: "Headline successfully #{item}", headline: article }, status: }
     end
   end
 
-  def handle_failed
-    log_error @article.errors.full_messages.join
+  def handle_failed(error_message)
+    Rails.logger.error error_message
+
     respond_to do |format|
       format.html do
-        flash.now[:error] = message
+        flash.now[:error] = error_message
         render "new"
       end
-      format.json { render json: { error: @article.errors.full_messages.join }, status: :unprocessable_entity }
+
+      format.json { render json: { error: error_message }, status: :unprocessable_entity }
     end
   end
 
-  def handle_not_found
-    log_error "Headline not found"
+  def handle_not_found(message)
+    Rails.logger.error message
     respond_to do |format|
       format.html do
         flash[:error] = message
         redirect_to articles_url
       end
-      format.json { render json: { error: "Headline not found" }, status: :unprocessable_entity }
+      format.json { render json: { error: message }, status: :unprocessable_entity }
     end
-  end
-
-  def log_error(message)
-    Rails.logger.error(message)
   end
 
   protected
