6c12fa1ed1486f95599e4d97cb331a168d588ffd
TRUETECHCODE <terveraosu@gmail.com>
2023-07-14 11:47:35 +0100

    Refactor code to improve code coverage

--- spec/services/news_api_spec.rb
+++ spec/services/news_api_spec.rb
@@ -4,48 +4,31 @@ require "rails_helper"
 
 RSpec.describe NewsApi do
   describe "#call" do
-    let(:data) do
-      [
-        {
-          source: { id: "associated-press", name: "Associated Press" },
-          author: "Susie Blann",
-          title: "Russian private army head claims control of Bakhmut but Ukraine says fighting continues - The
-          Associated Press",
-          description: "KYIV, Ukraine (AP) — The head of the Russian private army Wagner claimed Saturday that his
-          forces have taken control of the city of Bakhmut after the longest and most grinding battle of the
-          Russia-Ukraine war, but Ukrainian defense officials denied it.",
-          url: "https://apnews.com/article/bakhmut-russia-ukraine-wagner-prigozhin-da2fc05b818b3dcc39decd40b17d2d8b",
-          url_to_image: "https://storage.googleapis.com/afs-prod/media/8dd9fdbbef9d4727a480b275e08fe599/2879.webp",
-          publish_at: "2023-05-20T15:37:42Z",
-          content: "KYIV, Ukraine (AP) The head of the Russian private army Wagner claimed Saturday that his forces
-          have taken control of the city of Bakhmut after the longest and most grinding battle of the Russia-Ukra…"
-        },
-        {
-          source: { id: nil, name: "POLITICO.eu" },
-          author: "Ashleigh Furlong",
-          title: "Russia warns West sending F-16s to Ukraine 'carries enormous risks': TASS - POLITICO Europe",
-          description: "Kyiv has pushed its allies to supply modern combat aircraft for Ukraine’s defense against the
-      Russian invasion.",
-          url: "https://www.politico.eu/article/russia-alexander-grushko-warns-west-f16-jets-ukraine-carries-enormous-risks-tass/",
-          url_to_image: "https://www.politico.eu/cdn-cgi/image/width=1200,height=630,fit=crop,quality=80,onerror=redirect/wp-content/uploads/2023/05/20/GettyImages-1488520640-scaled.jpg",
-          publish_at: "2023-05-20T15:22:00Z",
-          content: "The Wests effort to potentially send modern fighter jets to Ukraine carries enormous risks, Russias
-      Deputy Foreign Minister Alexander Grushko warned on Saturday, according to Russian state news agenc…"
-        }
-      ]
-    end
+    let(:data) { build_list(:populated_article, 2) }
+
     let(:instance) { described_class.new("us") }
 
-    let(:articles) do
-      stub_request(:get, "https://newsapi.org/v2/top-headlines?apiKey=#{ENV.fetch('NEWSAPI_KEY', nil)}&country=us")
-        .with(
-          headers: {
-            "Accept" => "*/*",
-            "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-            "User-Agent" => "Ruby"
-          }
-        )
-        .to_return(
+    context "when API response returns success" do
+      let(:articles) do
+        stub_request(:get, "https://newsapi.org/v2/top-headlines?apiKey=#{ENV.fetch('NEWSAPI_KEY', nil)}&country=us")
+          .with(
+            headers: {
+              "Accept" => "*/*",
+              "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+              "User-Agent" => "Ruby"
+            }
+          )
+          .to_return(
+            status: 200,
+            body: {
+              status: "ok",
+              totalResults: 0,
+              articles: data
+            }.to_json,
+            headers: { "Content-Type": "application/json" }
+          )
+
+        allow(instance).to receive(:call).and_return(
           status: 200,
           body: {
             status: "ok",
@@ -55,58 +38,77 @@ RSpec.describe NewsApi do
           headers: { "Content-Type": "application/json" }
         )
 
-      allow(instance).to receive(:call).and_return(
-        status: 200,
-        body: {
-          status: "ok",
-          totalResults: 0,
-          articles: data
-        }.to_json,
-        headers: { "Content-Type": "application/json" }
-      )
+        response = instance.call
+        JSON.parse(response[:body])["articles"]
+      end
 
-      response = instance.call
-      JSON.parse(response[:body])["articles"]
+      let(:articles_empty) do
+        allow(instance).to receive(:call).and_return(
+          status: 200,
+          body: {
+            status: "ok",
+            totalResults: 0,
+            articles: []
+          }.to_json,
+          headers: { "Content-Type": "application/json" }
+        )
+
+        response = instance.call
+        JSON.parse(response[:body])["articles"]
+      end
+
+      it "increment articles by 1" do
+        expect(articles.length).to eq(2)
+      end
+
+      it "returns a successful response with articles" do
+        expect(articles[1]["author"]).to eq("Susie Blann")
+      end
+
+      it "returns an empty array if the API response has no articles" do
+        expect(articles_empty).to be_empty
+      end
     end
 
-    let(:articles_empty) do
-      allow(instance).to receive(:call).and_return(
-        status: 200,
-        body: {
-          status: "ok",
-          totalResults: 0,
-          articles: []
-        }.to_json,
-        headers: { "Content-Type": "application/json" }
-      )
+    context "when API response returns error" do
+      let(:articles_error) do
+        allow(instance).to receive(:call).and_return(
+          status: 500,
+          body: "Internal Server Error"
+        )
 
-      response = instance.call
-      JSON.parse(response[:body])["articles"]
-    end
-    let(:articles_error) do
-      allow(instance).to receive(:call).and_return(
-        status: 500,
-        body: "Internal Server Error"
-      )
+        response = instance.call
+        response[:body]["articles"]
+      end
 
-      response = instance.call
-      response[:body]["articles"]
-    end
+      before do
+        stub_request(:get, "https://newsapi.org/v2/top-headlines?apiKey=#{ENV.fetch('NEWSAPI_KEY', nil)}&country=us")
+          .with(
+            headers: {
+              "Accept" => "*/*",
+              "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+              "User-Agent" => "Ruby"
+            }
+          )
+          .to_return(
+            status: 500,
+            body: {
+              status: "error",
+              message: "API error message"
+            }.to_json,
+            headers: { "Content-Type": "application/json" }
+          )
+      end
 
-    it "increment articles by 1" do
-      expect(articles.length).to eq(2)
-    end
+      it "returns nil" do
+        expect(articles_error).to be_nil
+      end
 
-    it "returns a successful response with articles" do
-      expect(articles[1]["author"]).to eq("Ashleigh Furlong")
-    end
-
-    it "returns an empty array if the API response has no articles" do
-      expect(articles_empty).to be_empty
-    end
-
-    it "handles API errors and returns nil" do
-      expect(articles_error).to be_nil
+      it "logs the error message" do
+        allow(Rails.logger).to receive(:error)
+        instance.call
+        expect(Rails.logger).to have_received(:error).with(/500: API error message/)
+      end
     end
   end
 end
