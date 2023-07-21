d9d958dca341e6c15f23ef8fc12c0b02ca2aa198
TRUETECHCODE <terveraosu@gmail.com>
2023-07-21 13:27:50 +0100

    Update migration file to correct an issue raised during deployment

--- db/migrate/20230609170034_add_country_and_country_code_to_users.rb
+++ db/migrate/20230609170034_add_country_and_country_code_to_users.rb
@@ -4,13 +4,11 @@
 # It also creates indexes on the `country` and `country_code` columns for faster queries.
 class AddCountryAndCountryCodeToUsers < ActiveRecord::Migration[7.0]
   def change
-    safety_assured do
-      bulk_change_table :users, bulk: true do |t|
-        t.column :country, :string
-        t.column :country_code, :string
-        t.index :country
-        t.index :country_code
-      end
+    change_table :users, bulk: true do |t|
+      t.string :country
+      t.string :country_code
+      t.index :country
+      t.index :country_code
     end
   end
 end
